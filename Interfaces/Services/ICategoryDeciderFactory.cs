ebf263edc5ac9661add6102d4bef14c0b5ae8578
ThatOneGuyYouDoNotKnow <rene.heidrich-privat@outlook.de>
2018-06-22 23:42:00 +0200

    Code Cleanup

--- Interfaces/Services/ICategoryDeciderFactory.cs
+++ Interfaces/Services/ICategoryDeciderFactory.cs
@@ -5,7 +5,8 @@ namespace Interfaces.Services
 {
     public interface ICategoryDeciderFactory
     {
-        ICategoryDecider GetHowToDecideOnACategory(IField field, IPlayer currentPlayer,
+        ICategoryDecider GetHowToDecideOnACategory(
+            IField field, IPlayer currentPlayer,
             IDictionary<IPlayer, IScore> scores);
     }
 }
